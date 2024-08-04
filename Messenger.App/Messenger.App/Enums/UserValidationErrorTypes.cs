8679bc85393daa51ba43581900c2c63aaaa2a049
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-04 17:08:18 +0200

    Refactoring (adding useless controllers, updating appsettings, enums, ...)

---
+++ Messenger.App/Messenger.App/Enums/UserValidationErrorTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace Messenger.App.Enums
+{
+	public enum UserValidationErrorTypes
+	{
+		EmailAlreadyExists = 1,
+		UsernameAlreadyExists = 2,
+		InvalidEmail = 3,
+		InvalidUsername = 4,
+		InvalidPassword = 5,
+		UserNotFound = 6
+	}
+}
---
+++ Messenger.App/Messenger.App/MappingProfiles/UserMappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Messenger.App.Models;
+using Messenger.App.Models.DB_Models;
+
+namespace Messenger.App.MappingProfiles
+{
+	public class UserMappingProfiles : Profile
+	{
+        public UserMappingProfiles()
+        {
+			CreateMap<User, UserModel>()
+				.ForMember(t => t.UserId, m => m.MapFrom(t => t.u_id))
+				.ForMember(t => t.Email, m => m.MapFrom(t => t.u_email))
+				.ForMember(t => t.UserName, m => m.MapFrom(t => t.u_name))
+				.ForMember(t => t.UserId, m => m.MapFrom(t => t.u_id))
+				.ReverseMap();
+		}
+    }
+}
