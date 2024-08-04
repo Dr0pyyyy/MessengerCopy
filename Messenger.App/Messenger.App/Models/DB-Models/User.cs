6bd7084fc534ba9e70cd2242568ab1e5e9e69500
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-04 17:06:38 +0200

    Creating user classes

---
+++ Messenger.App/Messenger.App/Models/DB-Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.App.Models.DB_Models
+{
+	public class User
+	{
+        [Key]
+        public int u_id { get; set; }
+
+		[Required]
+        public string u_name { get; set; } = string.Empty;
+
+		[Required]
+		[EmailAddress]
+		public string u_email { get; set; } = string.Empty;
+
+		[Required]
+		public string u_password_hash { get; set; } = string.Empty;
+
+		[Required]
+		public int u_mobile_phone { get; set; }
+    }
+}
---
+++ Messenger.App/Messenger.App/Models/NewUserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Messenger.App.Models
+{
+	public class NewUserModel
+	{
+        public string Username { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int MobilePhone { get; set; }
+    }
+}
