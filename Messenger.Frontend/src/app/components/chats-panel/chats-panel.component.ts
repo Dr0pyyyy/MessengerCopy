3bec2f95d75d218609d0584831cbdc3e099c12f5
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-20 18:09:22 +0200

    Adding chats panel component

---
+++ Messenger.Frontend/src/app/components/chats-panel/chats-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './chats-panel.component.html',
+  styleUrl: './chats-panel.component.scss'
+})
+export class ChatsPanelComponent {
+
+}
