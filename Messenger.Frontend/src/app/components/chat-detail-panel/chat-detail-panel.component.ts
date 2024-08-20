cd59d58db32d7a0abd4c536303d530dadc20355b
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-20 18:09:41 +0200

    Adding chat-detail-panel.component

---
+++ Messenger.Frontend/src/app/components/chat-detail-panel/chat-detail-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-detail-panel.component.html',
+  styleUrl: './chat-detail-panel.component.scss'
+})
+export class ChatDetailPanelComponent {
+
+}
