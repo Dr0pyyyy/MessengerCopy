4a4ce50c0c05de4677cf576a528baf08982945d5
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-26 14:42:32 +0200

    Creating chats-panel component

--- Messenger.Frontend/src/app/components/chats-panel/chats-panel.component.ts
+++ Messenger.Frontend/src/app/components/chats-panel/chats-panel.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {ChatComponent} from "./chat/chat.component";
+import {UserModel} from "../../models/user.model";
+import {ChatService} from "../../services/chat.service";
+import {Subscription} from "rxjs";
+import {ChatModel} from "../../models/chat-model";
 
 @Component({
   selector: 'app-chats-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ChatComponent,
+    NgForOf
+  ],
   templateUrl: './chats-panel.component.html',
   styleUrl: './chats-panel.component.scss'
 })
-export class ChatsPanelComponent {
+export class ChatsPanelComponent implements OnInit, OnDestroy, OnChanges {
+
+  @Input() user: UserModel;
+
+  public chats: ChatModel[];
+
+  //Subscriptions
+  private chatServiceGetAllSubscription$: Subscription;
+
+  constructor(
+    private _chatService: ChatService
+  ) {
+  }
+
+  ngOnInit() {
+    this.chats = [];
+  }
+
+  ngOnDestroy() {
+    this.chatServiceGetAllSubscription$?.unsubscribe();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['user'] && this.user) {
+      this.openNewMessageTemplate();
+    }
+  }
+
+  openNewMessageTemplate() {
+    this.chatServiceGetAllSubscription$ = this._chatService.getAll(this.user.userId)
+      .subscribe({
+        next: (response) => {
+          this.chats = response;
+        },
+        error: (error) => {
+
+        }
+      })
+  }
+
 
 }
