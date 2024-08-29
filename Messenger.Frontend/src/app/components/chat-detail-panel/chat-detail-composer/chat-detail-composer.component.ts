c006e3a2f59f8f77a2d4de2f4ef73db1e0844a3d
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-29 19:28:04 +0200

    creating chat-detail-composer.component

---
+++ Messenger.Frontend/src/app/components/chat-detail-panel/chat-detail-composer/chat-detail-composer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail-composer',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-detail-composer.component.html',
+  styleUrl: './chat-detail-composer.component.scss'
+})
+export class ChatDetailComposerComponent {
+
+}
