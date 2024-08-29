8073c5c1c8b94e9316f302bf2145daa955820e71
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-29 19:27:42 +0200

    Creating chat-detail-composer component

---
+++ Messenger.Frontend/src/app/components/chat-detail-panel/chat-detail-thread/chat-detail-thread.component.ts
@@ -0,0 +1,11 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail-thread',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-detail-thread.component.html',
+  styleUrl: './chat-detail-thread.component.scss'
+})
+export class ChatDetailThreadComponent {
+}
