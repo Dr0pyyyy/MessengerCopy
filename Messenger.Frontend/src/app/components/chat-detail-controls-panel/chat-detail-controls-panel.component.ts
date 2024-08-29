55de7bf5aa51d8a1e32d489a46e8ac19fa8b08ce
Dr0pyyyy <adamkoutny12345@seznam.cz>
2024-08-29 19:28:18 +0200

    creating chat-detail-controls-panel.component

---
+++ Messenger.Frontend/src/app/components/chat-detail-controls-panel/chat-detail-controls-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail-controls-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-detail-controls-panel.component.html',
+  styleUrl: './chat-detail-controls-panel.component.scss'
+})
+export class ChatDetailControlsPanelComponent {
+
+}
